41f5e45bca03ebcecc95130ce4862b8755153cd6
brown46 <rlawlsxo623@naver.com>
2023-02-16 15:16:01 +0900

    회원가입 자바스크립트

--- WebContent/resource/js/myJavascript.js
+++ WebContent/resource/js/myJavascript.js
@@ -1,1 +1,19 @@
-alert("123");
+window.onload= function validCheck(){
+    var form = document.forms[0];
+    form.id.addEventListener("input", function(){
+        if(form.id.nextElementSibling!==null){
+            form.id.nextElementSibling.remove();
+        }
+        if(form.id.valueOf.length<6||form.id.valueOf.length>12){
+            var error= createErrormessage("6~12 사이");
+            form.id.after(error);
+        }
+    })
+}
+
+
+function createErrormessage(message){
+    var div= document.createElement("div");
+    div.innerText=message;
+    return div;
+}
